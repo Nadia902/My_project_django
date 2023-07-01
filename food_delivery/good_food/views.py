a0e3f148facaa2e382c6bd52dc9ad393927565ed
Nadia <Nadia90291@gmail.com>
2023-07-01 12:51:50 +0300

    new_save

--- food_delivery/good_food/views.py
+++ food_delivery/good_food/views.py
@@ -4,5 +4,18 @@ from .models import BeFit
 
 def projects(request):
     prof = BeFit.objects.all()
-    context = {'profiles': prof}
-    return render(request, 'good_food/projects.html', context)
+    gender = int(request.GET.get('calc_gender'))
+    goal = float(request.GET.get('calc_goal'))
+    act = float(request.GET.get('calc_act'))
+    age = float(request.GET.get('calc_age'))
+    height = float(request.GET.get('calc_height'))
+    weight = float(request.GET.get('calc_weight'))
+
+    if gender == 1:
+        control = round(((655.1 + (weight*9.563) + (height*1.85) - (age*4.676)) * act) + goal)
+        context = {'profiles': prof, 'control': control}
+        return render(request, 'good_food/projects.html', context)
+    elif gender == 2:
+        control = round(((66.5 + (weight*13.75) + (height*5.003) - (age*6.775)) * act) + goal)
+        context = {'profiles': prof, 'control': control}
+        return render(request, 'good_food/projects.html', context)
