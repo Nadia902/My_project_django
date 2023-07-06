f1dd6bc1a4cb38801dc042d0b58c36ec4df1bf20
Nadia <Nadia90291@gmail.com>
2023-07-06 22:33:07 +0300

    new

--- food_delivery/good_food/views.py
+++ food_delivery/good_food/views.py
@@ -3,19 +3,25 @@ from .models import BeFit
 
 
 def projects(request):
+    control = 0
     prof = BeFit.objects.all()
-    gender = int(request.GET.get('calc_gender'))
-    goal = float(request.GET.get('calc_goal'))
-    act = float(request.GET.get('calc_act'))
-    age = float(request.GET.get('calc_age'))
-    height = float(request.GET.get('calc_height'))
-    weight = float(request.GET.get('calc_weight'))
+    gender = request.GET.get('calc_gender')
+    goal = request.GET.get('calc_goal')
+    act = request.GET.get('calc_act')
+    age = request.GET.get('calc_age')
+    height = request.GET.get('calc_height')
+    weight = request.GET.get('calc_weight')
 
-    if gender == 1:
-        control = round(((655.1 + (weight*9.563) + (height*1.85) - (age*4.676)) * act) + goal)
+    if gender == "1":
+        control = round(((655.1 + (float(weight)*9.563) + (float(height)*1.85) -
+                          (float(age)*4.676)) * float(act)) + float(goal))
         context = {'profiles': prof, 'control': control}
         return render(request, 'good_food/projects.html', context)
-    elif gender == 2:
-        control = round(((66.5 + (weight*13.75) + (height*5.003) - (age*6.775)) * act) + goal)
+    elif gender == "2":
+        control = round(((66.5 + (float(weight)*13.75) + (float(height)*5.003) -
+                                  (float(age)*6.775)) * float(act)) + float(goal))
         context = {'profiles': prof, 'control': control}
         return render(request, 'good_food/projects.html', context)
+
+    context = {'profiles': prof, 'control': control}
+    return render(request, 'good_food/projects.html', context)
