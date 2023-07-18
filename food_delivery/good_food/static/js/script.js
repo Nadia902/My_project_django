69612af1d838f76210d9f414e8e1d2e0f8a92c9c
Nadia <Nadia90291@gmail.com>
2023-07-19 00:05:42 +0300

    new_save

--- food_delivery/good_food/static/js/script.js
+++ food_delivery/good_food/static/js/script.js
@@ -1,10 +1,13 @@
-let slides = document.querySelectorAll(".program-group__el");
+let slides = document.querySelectorAll(".round");
+let eat = document.querySelectorAll(".eat_flex");
 
-for(let i = 0; i< slides.length; i++) {
-    slides[i].addEventListener('click', () => {
-        clearActiveClasses()
-        slides[i].classList.add('active');
-    });
+function clear(){
+    for(let i = 0; i < slides.length; i++) {
+        slides[i].addEventListener('click', () => {
+            clearActiveClasses()
+            slides[i].classList.add('active')
+        });
+    }
 }
 
 function clearActiveClasses(){
@@ -13,17 +16,135 @@ function clearActiveClasses(){
     }
 }
 
-let slides_two = document.querySelectorAll(".f-column");
+clear()
 
-for(let i = 0; i< slides_two.length; i++) {
-    slides_two[i].addEventListener('click', () => {
-        clearActiveClass()
-        slides_two[i].classList.add('active_two');
-    });
+function light(){
+    for(let i = 0; i < eat.length; i++) {
+        eat[i].classList.add('eatnone2')
+        }
+    let li = document.querySelectorAll(".lightx")
+    for(let j = 0; j < li.length; j++) {
+        li[j].classList.remove('eatnone2')
+    }
 }
 
-function clearActiveClass(){
-    for(let i = 0; i< slides_two.length; i++) {
-        slides_two[i].classList.remove('active_two');
+function normal(){
+    for(let i = 0; i < eat.length; i++) {
+        eat[i].classList.add('eatnone2')
+        }
+    let no = document.querySelectorAll(".normalx")
+    for(let j = 0; j < no.length; j++) {
+        no[j].classList.remove('eatnone2')
+    }
+}
+
+function strong(){
+    for(let i = 0; i < eat.length; i++) {
+        eat[i].classList.add('eatnone2')
+        }
+    let st = document.querySelectorAll(".strongx")
+    for(let j = 0; j < st.length; j++) {
+        st[j].classList.remove('eatnone2')
+    }
+}
+
+function superstrong(){
+    for(let i = 0; i < eat.length; i++) {
+        eat[i].classList.add('eatnone2')
+        }
+    let su = document.querySelectorAll(".superstrongx")
+    for(let j = 0; j < su.length; j++) {
+        su[j].classList.remove('eatnone2')
+    }
+}
+
+function super1(){
+    for(let i = 0; i < eat.length; i++) {
+        eat[i].classList.add('eatnone2')
+        }
+    let sup = document.querySelectorAll(".superx")
+    for(let j = 0; j < sup.length; j++) {
+        sup[j].classList.remove('eatnone2')
+    }
+}
+
+let food = document.querySelectorAll(".foodflex");
+let column = document.querySelectorAll(".f-column");
+
+function cleartwo(){
+    for(let i = 0; i < column.length; i++) {
+        column[i].addEventListener('click', () => {
+            clearActiveClassestwo()
+            column[i].classList.add('active_two')
+        });
+    }
+}
+
+function clearActiveClassestwo(){
+    for(let i = 0; i< column.length; i++) {
+        column[i].classList.remove('active_two');
+    }
+}
+
+cleartwo()
+
+function monday(){
+    for(let i = 0; i < food.length; i++) {
+        food[i].classList.add('eatnone')
+        }
+    let m = document.querySelectorAll(".monday")
+    for(let j = 0; j < m.length; j++) {
+        m[j].classList.remove('eatnone')
+    }
+}
+
+function tuesday(){
+    for(let i = 0; i < food.length; i++) {
+        food[i].classList.add('eatnone')
+        }
+    let t = document.querySelectorAll(".tuesday")
+    for(let j = 0; j < t.length; j++) {
+        t[j].classList.remove('eatnone')
+    }
+}
+function wednesday(){
+    for(let i = 0; i < food.length; i++) {
+        food[i].classList.add('eatnone')
+        }
+    let w = document.querySelectorAll(".wednesday")
+    for(let j = 0; j < w.length; j++) {
+        w[j].classList.remove('eatnone')
+    }
+}
+function thursday(){
+    for(let i = 0; i < food.length; i++) {
+        food[i].classList.add('eatnone')}
+    let h = document.querySelectorAll(".thursday")
+    for(let j = 0; j < h.length; j++) {
+        h[j].classList.remove('eatnone')
+    }
+}
+function friday(){
+    for(let i = 0; i < food.length; i++) {
+        food[i].classList.add('eatnone')}
+    let f = document.querySelectorAll(".friday")
+    for(let j = 0; j < f.length; j++) {
+        f[j].classList.remove('eatnone')
+    }
+}
+function saturday(){
+    for(let i = 0; i < food.length; i++) {
+        food[i].classList.add('eatnone')}
+    let t = document.querySelectorAll(".saturday")
+    for(let j = 0; j < t.length; j++) {
+        t[j].classList.remove('eatnone')
+    }
+}
+function sunday(){
+    for(let i = 0; i < food.length; i++) {
+        food[i].classList.add('eatnone')}
+    let d = document.querySelectorAll(".sunday")
+    for(let j = 0; j < d.length; j++) {
+        d[j].classList.remove('eatnone')
     }
 }
--- food_delivery/good_food/views.py
+++ food_delivery/good_food/views.py
@@ -6,6 +6,10 @@ def projects(request):
     control = 0
     prof = BeFit.objects.all()
     prof_light = Light.objects.all()
+    prof_normal = Normal.objects.all()
+    prof_strong = Strong.objects.all()
+    prof_superstrong = SuperStrong.objects.all()
+    prof_super = Super.objects.all()
     gender = request.GET.get('calc_gender')
     goal = request.GET.get('calc_goal')
     act = request.GET.get('calc_act')
@@ -16,13 +20,11 @@ def projects(request):
     if gender == "1":
         control = round(((655.1 + (float(weight)*9.563) + (float(height)*1.85) -
                           (float(age)*4.676)) * float(act)) + float(goal))
-        context = {'profiles': prof, 'control': control, 'prof_light': prof_light}
-        return render(request, 'good_food/projects.html', context)
     elif gender == "2":
         control = round(((66.5 + (float(weight)*13.75) + (float(height)*5.003) -
-                                  (float(age)*6.775)) * float(act)) + float(goal))
-        context = {'profiles': prof, 'control': control, 'prof_light': prof_light}
-        return render(request, 'good_food/projects.html', context)
+                          (float(age)*6.775)) * float(act)) + float(goal))
 
-    context = {'profiles': prof, 'control': control, 'prof_light': prof_light}
+    context = {'profiles': prof, 'control': control, 'prof_light': prof_light,
+               'prof_normal': prof_normal, 'prof_strong': prof_strong,
+               'prof_superstrong': prof_superstrong, 'prof_super': prof_super}
     return render(request, 'good_food/projects.html', context)
